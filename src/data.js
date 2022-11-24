af2271585abb86718bd8da9caee4f702dfbb1f77
Ajay-Goswmai <goswamiajay2002@gmail.com>
2022-11-24 19:10:35 +0530

    frontend-part-of-ecommerce-site

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://cdna.lystit.com/photos/571b-2014/12/02/tommy-hilfiger-black-core-regatta-jacket-product-1-17634763-1-240866964-normal.jpeg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/originals/72/45/09/7245098e7caa8b13d36e95de27c1d4ee.jpg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "http://res.freestockphotos.biz/pictures/14/14842-a-beautiful-young-woman-in-business-attire-pv.jpg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://th.bing.com/th/id/OIP.y-tTvGUWawRSR5BJl5kXAQHaHa?pid=ImgDet&rs=1",
+    },
+    {
+      id:3,
+      img:"https://th.bing.com/th/id/OIP.FViqANwHlfUDGdA5Wtm6fgHaId?pid=ImgDet&rs=1",
+    },
+    {
+      id:4,
+      img:"https://i.pinimg.com/originals/e7/13/97/e713978ff0a4391f0865f1d978c243de.jpg",
+    },
+    {
+      id:5,
+      img:"https://th.bing.com/th/id/OIP.vC4mc-jPkYD3WansIwW9ZAHaHa?pid=ImgDet&rs=1",
+    },
+    {
+      id:6,
+      img:"https://images.puma.net/images/023148/01/fnd/AUS/w/1000/h/1000/bg/255,255,255",
+    },
+    {
+      id:7,
+      img:"https://th.bing.com/th/id/OIP.Du0taFTH2u78gaJ49ArC0gHaHa?pid=ImgDet&w=1065&h=1065&rs=1",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+    {
+      id:9,
+      img:"https://www.babyshop.com/images/492417/x_large.jpg",
+    },
+    {
+      id:10,
+      img:"https://th.bing.com/th/id/OIP._qopsnTcetI26M9ByWzBzAHaHa?pid=ImgDet&rs=1",
+    },
+    {
+      id:11,
+      img:"https://images-na.ssl-images-amazon.com/images/I/91zStr6tBWL._AC_UL1500_.jpg",
+    },
+    {
+      id:12,
+      img:"https://i.pinimg.com/originals/ae/c7/26/aec7267036ca163c8f23b8b4da9f23f9.jpg",
+    },
+  ]
